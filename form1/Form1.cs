9e8791448b8dd52f9bb13c49b5b197c1567525e6
Bolark <81440598+Bolark@users.noreply.github.com>
2024-01-09 14:05:48 +0300

    Form1.cs

--- form1/Form1.cs
+++ form1/Form1.cs
@@ -31,7 +31,6 @@ namespace form1
         {
             InitializeComponent();
 
-            // LabelMousePosition kontrolünü form üzerine ekleyin
             labelMousePosition = new Label();
             labelMousePosition.Location = new System.Drawing.Point(10, 10);
             this.Controls.Add(labelMousePosition);
@@ -114,28 +113,22 @@ namespace form1
 
                 yesilAltigen.Paint += DrawHexagon;
                 this.Controls.Add(yesilAltigen);
-                // Panel'i sürükleyebilir hale getir
                 yesilAltigen.Invalidate();
             }
         }
 
         private void DrawHexagon(object sender, PaintEventArgs e)
         {
-            // Altıgenin kenar uzunluğu
             int sideLength = 50;
 
             var aaa = new Form1();
-            // Altıgenin merkezi
             int centerX = aaa.Width / 2;
             int centerY = aaa.Height / 2;
 
-            // Altıgenin köşe noktalarını hesapla
             Point[] hexagonPoints = CalculateHexagonPoints(centerX, centerY, sideLength);
 
-            // Yeşil bir fırça oluştur
             using (Brush brush = new SolidBrush(Color.Green))
             {
-                // Altıgeni çiz
                 using (Graphics g = this.CreateGraphics())
                 {
                     g.FillPolygon(brush, hexagonPoints);
@@ -162,13 +155,9 @@ namespace form1
 
         private void PinkCircle_Paint(object sender, PaintEventArgs e)
         {
-            // Get the Graphics object from PaintEventArgs.
             Graphics g = e.Graphics;
-
-            // Get the current panel.
             Panel panel = (Panel)sender;
 
-            // Fill the ellipse (circle) with a color (you can customize the color).
             using (Brush brush = new SolidBrush(Color.DeepPink))
             {
                 g.FillEllipse(brush, 0, 0, panel.Width, panel.Height);
@@ -179,67 +168,24 @@ namespace form1
         {
             int sideLength = 50;
 
-            // Get the Graphics object from PaintEventArgs.
             Graphics g = e.Graphics;
 
-            // Get the current panel.
             Panel panel = (Panel)sender;
 
-            Point point1 = new Point(0, panel.Height);  // Bottom-left corner
-            Point point2 = new Point(panel.Width, panel.Height);  // Bottom-right corner
-            Point point3 = new Point(panel.Width / 2, 0);  // Top-middle corner
+            Point point1 = new Point(0, panel.Height);
+            Point point2 = new Point(panel.Width, panel.Height);
+            Point point3 = new Point(panel.Width / 2, 0);
 
-            // Fill the triangle with a color (you can customize the color).
             using (Brush brush = new SolidBrush(Color.Blue))
             {
                 g.FillPolygon(brush, new Point[] { point1, point2, point3 });
             }
         }
 
-       
-
-
-
-
-        //private PaintEventHandler TrianglePanel_Paint(int mouseX, int mouseY)
-        //{
-        //    int sideLength = 50;
-
-        //    Point point1 = new Point(mouseX, mouseY);
-        //    Point point2 = new Point(mouseX + sideLength, mouseY);
-        //    Point point3 = new Point(mouseX + sideLength / 2, mouseY - (int)(Math.Sqrt(3) * sideLength / 2));
-
-        //    using (Graphics g = this.CreateGraphics())
-        //    {
-        //        g.FillPolygon(Brushes.Blue, new Point[] { point1, point2, point3 });
-
-        //        return g;
-        //    }
-        //}
-
-        //private void TrianglePanel_Paint(object sender, PaintEventArgs e)
-        //{
-        //    int sideLength = 50;
-
-        //    Point point1 = new Point(mouseX, mouseY);
-        //    Point point2 = new Point(mouseX + sideLength, mouseY);
-        //    Point point3 = new Point(mouseX + sideLength / 2, mouseY - (int)(Math.Sqrt(3) * sideLength / 2));
-
-        //    using (Graphics g = this.CreateGraphics())
-        //    {
-        //        g.FillPolygon(Brushes.Blue, new Point[] { point1, point2, point3 });
-        //    }
-        //}
-
         private void MainForm_MouseMove(object sender, MouseEventArgs e)
         {
-            //Fare pozisyonunu alan kod
             int mouseX = e.X;
             int mouseY = e.Y;
-
-            // Alınan pozisyonu kullanarak bir şeyler yapabilirsiniz
-            // Örneğin, pozisyonu bir etikete yazdırabilirsiniz
-            //labelMousePosition.Text = $"Mouse Pozisyonu: X={mouseX}, Y={mouseY}";
 
             label5.Text = $"Mouse Pozisyonu: X={mouseX}, Y={mouseY}";
 
@@ -295,7 +241,6 @@ namespace form1
 
         private Control FindControlByName(string name)
         {
-            // Form içinde ismi verilen kontrolü bul
 
             foreach (Control control in this.Controls)
             {
@@ -304,8 +249,6 @@ namespace form1
                     return control;
                 }
             }
-
-            // Eğer bulunamazsa null döndür
             return null;
         }
 
@@ -347,7 +290,6 @@ namespace form1
 
             if (mouseEventArgs != null)
             {
-                // sarı kareyi tıklanan noktanın konumuna ayarlayın
                 sarıKare.Location = new Point(mouseEventArgs.X + groupBox1.Left - sarıKare.Width / 2,
                                               mouseEventArgs.Y + groupBox1.Top - sarıKare.Height / 2);
      
@@ -757,155 +699,3 @@ namespace form1
         }
     }
 }
-//        public abstract class Shape
-//        {
-//            public Point StartPoint { get; set; }
-//            public Point EndPoint { get; set; }
-//            public Color Color { get; set; }
-
-//            public abstract void Draw(Graphics g);
-//        }
-
-//        public class RectangleShape : Shape
-//        {
-//            public override void Draw(Graphics g)
-//            {
-//                using (var brush = new SolidBrush(Color))
-//                {
-//                    g.FillRectangle(brush, StartPoint.X, StartPoint.Y, EndPoint.X - StartPoint.X, EndPoint.Y - StartPoint.Y);
-//                }
-//            }
-//        }
-
-//        public class EllipseShape : Shape
-//        {
-//            public override void Draw(Graphics g)
-//            {
-//                using (var brush = new SolidBrush(Color))
-//                {
-//                    g.FillEllipse(brush, StartPoint.X, StartPoint.Y, EndPoint.X - StartPoint.X, EndPoint.Y - StartPoint.Y);
-//                }
-//            }
-//        }
-
-//        public class DrawingApplication
-//        {
-//            private List<Shape> shapes;
-//            private Shape currentShape;
-//            private Color selectedColor;
-
-//            public Color SelectedColor { get; internal set; }
-
-//            public DrawingApplication()
-//            {
-//                shapes = new List<Shape>();
-//                selectedColor = Color.Black;
-//            }
-
-//            public void StartDrawing(Shape shape)
-//            {
-//                currentShape = shape;
-//            }
-
-//            public void EndDrawing()
-//            {
-//                shapes.Add(currentShape);
-//                currentShape = null;
-//            }
-
-//            public void DrawAll(Graphics g)
-//            {
-//                foreach (var shape in shapes)
-//                {
-//                    shape.Draw(g);
-//                }
-
-//                if (currentShape != null)
-//                {
-//                    currentShape.Draw(g);
-//                }
-//            }
-
-//            public void SetSelectedColor(Color color)
-//            {
-//                selectedColor = color;
-//                if (currentShape != null)
-//                {
-//                    currentShape.Color = selectedColor;
-//                }
-//            }
-//        }
-
-//        public partial class DrawingForm : Form
-//        {
-//            private DrawingApplication drawingApp;
-//            private Shape currentShape;
-
-//            public DrawingForm()
-//            {
-//                InitializeComponent();
-//                drawingApp = new DrawingApplication();
-//            }
-
-//            private void DrawingForm_MouseDown(object sender, MouseEventArgs e)
-//            {
-//                if (currentShape == null)
-//                {
-//                    if (e.X > ClientSize.Width - 200) // Sağ taraftaki menü alanının sınırları
-//                    {
-//                        if (e.Y < 50) // Çizim şekilleri bölgesi
-//                        {
-//                            if (e.Y < 10 + 25) // Dikdörtgen seçildi
-//                            {
-//                                currentShape = new RectangleShape();
-//                            }
-//                            else if (e.Y < 40 + 25) // Daire seçildi
-//                            {
-//                                currentShape = new EllipseShape();
-//                            }
-//                            // Diğer şekilleri buraya ekleyebilirsiniz
-//                            currentShape.Color = drawingApp.SelectedColor;
-//                            currentShape.StartPoint = e.Location;
-//                            drawingApp.StartDrawing(currentShape);
-//                        }
-//                        else if (e.Y > 150 && e.Y < 300) // Renk seçimi bölgesi
-//                        {
-//                            int colorIndex = (e.Y - 150) / 25;
-//                            Color selectedColor = GetColorByIndex(colorIndex);
-//                            drawingApp.SetSelectedColor(selectedColor);
-//                        }
-//                    }
-//                }
-//            }
-
-//            private void DrawingForm_MouseUp(object sender, MouseEventArgs e)
-//            {
-//                if (e.Button == MouseButtons.Left && currentShape != null)
-//                {
-//                    currentShape.EndPoint = e.Location;
-//                    drawingApp.EndDrawing();
-//                    currentShape = null;
-//                    Refresh();
-//                }
-//            }
-
-//            private void DrawingForm_Paint(object sender, PaintEventArgs e)
-//            {
-//                drawingApp.DrawAll(e.Graphics);
-//            }
-
-//            private Color GetColorByIndex(int index)
-//            {
-//                // Renk seçimi için indeks bazlı renk listesi
-//                Color[] colors = { Color.Red, Color.Blue, Color.DarkGreen, Color.Orange, Color.Black, Color.Yellow, Color.Purple, Color.Maroon, Color.White };
-
-//                if (index >= 0 && index < colors.Length)
-//                {
-//                    return colors[index];
-//                }
-
-//                return Color.Black;
-//            }
-//        }
-//    }
-//}
